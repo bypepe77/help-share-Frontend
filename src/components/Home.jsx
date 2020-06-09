ea5a4fccad65f131441c0040c99657094f5f72f3
Mario Espada <mario.espada77@gmail.com>
2020-06-09 03:17:34 +0200

    ADD modal to add future posts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { IonApp, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonList, IonItemDivider, IonButton, IonFab, IonIcon , IonFabButton} from '@ionic/react';
+import React, { Component, useState } from 'react';
+import { IonApp,IonModal,  IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonList, IonItemDivider, IonButton, IonFab, IonIcon , IonFabButton} from '@ionic/react';
 import { add } from  'ionicons/icons';
 import { withAuth } from "../Context/AuthContext";
 import { Link } from "react-router-dom";
@@ -8,11 +8,24 @@ import { DivAddPost } from "../css/index";
 import { NavLink } from 'react-router-dom';
 
 class Home extends Component {
+    state = {
+        ShowModal: false,
+    }
     handleClick = () => {
         console.log("click");
         this.props.history.push("/profile/mario77"); // Test
     }
+
+    modalController = () =>{
+        const {ShowModal} = this.state;
+        console.log("click")
+        this.setState({
+            ShowModal: !ShowModal,
+        })
+    }
+
     render() {
+        const { ShowModal } = this.state;
         return (
             <IonPage>
                 <IonHeader>
@@ -24,11 +37,17 @@ class Home extends Component {
                     <p>Home</p>
                     <DivAddPost>
                         <IonFab vertical="bottom" horizontal="end" slot="fixed" style={{marginBottom: "60px"}}>
-                            <IonFabButton onClick={this.handleClick}>
+                            <IonFabButton onClick={this.modalController}>
                                 <IonIcon icon={add} />
                             </IonFabButton>
                         </IonFab>
                     </DivAddPost>
+                        <IonModal isOpen={ShowModal}>
+                            <p>Publicacion</p>
+                        <IonButton onClick={this.modalController}>Cerrar</IonButton>
+                     </IonModal>
+
+
                 </IonContent>
             </IonPage>
            
