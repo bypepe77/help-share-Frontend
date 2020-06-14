4da90b43e04caa9f964da2ee8fa1ce86ca0059db
Mario Espada <mario.espada77@gmail.com>
2020-06-15 01:57:51 +0200

    ADD first styles to post card

--- src/components/Posts/ListPosts.jsx
+++ src/components/Posts/ListPosts.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import "../../css/styles.css";
-
+import UserImage from "../../images/default.jpg";
+import { Link } from "react-router-dom";
 const ListPosts = (props) =>{
     const { post } = props;
     return (
         <div className="post-card">
-            <div className="user-info">
-                <p>{post.username.username}</p>
+            <div className="user-info" >
+                <img src={UserImage} width="40px" height="40px"/>
+                <Link to={`/profile/${post.username.username}`}>
+                    <p className="username-size">{post.username.username}</p>
+                </Link>
             </div>
-            <p>{post.text}</p>
+            <div>
+                <p className="post-text">{post.text}</p>
+            </div>
         </div>
       );
 }
