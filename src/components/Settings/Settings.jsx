554dd88b0d3a9ff5f53830c5cda055cb5b1dbc13
Mario Espada <mario.espada77@gmail.com>
2020-06-16 00:30:24 +0200

    ADD settings component

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { IonContent, IonHeader, IonLabel, IonTitle, IonToolbar} from '@ionic/react';
+
+const Settings = () =>{
+        return (
+            <div>
+                
+            </div>
+        );
+}
+
+export default Settings;
