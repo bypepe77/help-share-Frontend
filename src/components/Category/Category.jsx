dacb3088438e06d516a0c01b63946d212230466d
Mario Espada <mario.espada77@gmail.com>
2020-06-16 18:26:04 +0200

    ADD basic category page

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/react";
 
 const Category = props => {
     const { category } = props.match.params;
-    
+
     useEffect(() =>{
         console.log("Mounting");
     });
     return (
-        <div>
-            <h1>{category}</h1>
-        </div>
+        <IonPage>
+            <IonHeader>
+                 <IonToolbar>
+                    <IonTitle>{category}</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <h1>{category}</h1>
+            </IonContent>
+        </IonPage>
     );
 };
 
