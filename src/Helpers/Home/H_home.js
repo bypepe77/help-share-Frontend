8602f72170126c86fda9c80a48e54b6ee80fc6e9
Mario Espada <mario.espada77@gmail.com>
2020-06-16 16:12:40 +0200

    Change category name

--- src/Helpers/Home/H_home.js
+++ src/Helpers/Home/H_home.js
@@ -2,7 +2,7 @@ import React from "react"
 import Post from "../../components/Posts/Post";
 import "../../css/styles.css"
 
-const categories = ["TestSlide1", "TestSlide2", "TestSlide3", "TestSlide4", "TestSlide5", "TestSlide6", "TestSlide7", "TestSlide8"];
+const categories = ["Categoría", "Categoría", "Categoría", "Categoría", "Categoría", "Categoría", "Categoría", "Categoría"];
 
 const PostStatus = (posts) => {
     if(posts.length === 0 ){
