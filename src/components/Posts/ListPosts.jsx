ded7a209f9eb9b55d844a437911208a55993c534
Mario Espada <mario.espada77@gmail.com>
2020-06-20 16:38:52 +0200

    MOVE function existLink to a new File

--- src/components/Posts/ListPosts.jsx
+++ src/components/Posts/ListPosts.jsx
@@ -10,29 +10,7 @@ import { ReactTinyLink } from 'react-tiny-link'
 const ListPosts = (props) =>{
     const { post } = props;
     const DateToFormat = post.created_at;
-    const ExistLink = (text) =>{
-       const urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-       const getUrl =  text.match(urlRegex, (url) =>{
-           return url
-       })
-       const deleteUrl = text.replace(urlRegex, "");
-       if(getUrl){
-             return ( 
-                <div>
-                    <p className="post-text">{deleteUrl}</p>
-                    <ReactTinyLink
-                        cardSize="small"
-                        showGraphic={true}
-                        maxLine={3}
-                        minLine={3}
-                        url={getUrl.toString()}
-                    />
-                </div>
-            )
-         }else{
-             return <p className="post-text">{deleteUrl}</p>
-        }
-    }
+    
     return (
         <div className="post-card">
             <div className="user-info" >
