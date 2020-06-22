9dfb61c37c7619f3fe26a7c41a50d0b52e93f5f2
Mario Espada <mario.espada77@gmail.com>
2020-06-22 19:28:23 +0200

    Fix error in last post

--- src/components/Posts/Post.jsx
+++ src/components/Posts/Post.jsx
@@ -4,13 +4,13 @@ import ListPosts from "./ListPosts";
 const Post = (props) =>{
     const { posts } = props;
     return (
-        <>
+        <div className="post-box">
             {posts.map(post => {
                 return (
                     <ListPosts key={`id-post-${post._id}`} post={post}></ListPosts>
                 );
             })}
-        </>
+        </div>
       );
 }
 
